d3749fa07602f655e2249d66d184beca2389e249
Mr.ture.China <mr.true.china@gmail.com>
2021-09-19 09:27:29 +0800

    初始化

---
+++ Event-Control-Lib-Sources/EventControlLib.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace MtC.Mod.ChineseParents.EventControlLib
+{
+    public static class Main
+    {
+        /// <summary>
+        /// Mod 对象
+        /// </summary>
+        public static UnityModManager.ModEntry ModEntry { get; set; }
+
+        /// <summary>
+        /// 这个 Mod 是否启动
+        /// </summary>
+        public static bool enabled;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            // 保存 Mod 对象
+            ModEntry = modEntry;
+            ModEntry.OnToggle = OnToggle;
+
+            // 加载 Harmony
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            modEntry.Logger.Log("事件控制前置 Mod 加载完成");
+
+            // 返回加载成功
+            return true;
+        }
+
+        /// <summary>
+        /// Mod Manager 对 Mod 进行控制的时候会调用这个方法
+        /// </summary>
+        /// <param name="modEntry"></param>
+        /// <param name="value">这个 Mod 是否激活</param>
+        /// <returns></returns>
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            // 将 Mod Manager 切换的状态保存下来
+            enabled = value;
+
+            // 返回 true 表示这个 Mod 切换到 Mod Manager 切换的状态，返回 false 表示 Mod 依然保持原来的状态
+            return true;
+        }
+    }
+
+    ////////--------////////--------//////// 测试功能 ////////--------////////--------////////
+
+    /// <summary>
+    /// 发起对话方法
+    /// </summary>
+    [HarmonyPatch(typeof(chat_manager), "start_chat")]
+    public static class record_manager_saverecord
+    {
+        private static void Prefix(chat_manager __instance, int id)
+        {
+            // 如果 Mod 未启动则直接按照游戏原本的逻辑进行调用
+            if (!Main.enabled)
+            {
+                return;
+            }
+
+            TipsManager.instance.AddTips("ID 为 " + id + " 的对话即将开始", 1);
+
+            Main.ModEntry.Logger.Log("ID 为 " + id + " 的对话即将开始");
+        }
+    }
+}
